cc050d3f0c0812a5513bc5b5fc724b7ed6111b04
SamGerber <gerbyxvi@gmail.com>
2016-01-15 15:35:32 -0800

    Implement hashing functions

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -4,15 +4,18 @@ end
 
 class Array
   def hash
+    self.to_s.hash
   end
 end
 
 class String
   def hash
+    self.unpack("s5").map{|el| el.nil? ? 1.hash : el.hash}.inject(:+)
   end
 end
 
 class Hash
   def hash
+    self.to_a.flatten.map{|i| i.to_s}.sort.hash
   end
 end
